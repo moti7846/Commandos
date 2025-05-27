b4c9ed624f5d03a6d064048b511555bd4632cead
moti7846 <8563816@gmail.com>
2025-05-27 09:43:44 +0300

    Add project files.

---
+++ Commandos/Commando.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class Commando
+    {
+        public string Name;
+        public string NameCode;
+        public string Tools;
+    }
+}
